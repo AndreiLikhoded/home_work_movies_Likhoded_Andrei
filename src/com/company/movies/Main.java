4f77d67f94eee9ff0613e1ae53b21163e59f986f
Andrei Likhoded <liberty102and8131186@gmail.com>
2022-06-30 11:39:59 +0600

    some methods were added

--- src/com/company/movies/Main.java
+++ src/com/company/movies/Main.java
@@ -5,27 +5,66 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args){
-        run();
+        runYear();
+        runReverse();
+        runName();
+        runReversedName();
     }
-    public static void run(){
+    public static void runYear(){
+
+        System.out.println("----------------------------------------YearOfProduction------------------------");
         Map<String, List<Movie>> movieList = MovieService.readFile();
         List<Movie> movie = movieList.get("movies");
         Comparator<Movie> cmp = Comparator.comparingInt(Movie::getYear);
-//        Comparator<Movie> cmp1 = Comparator.comparing(Movie::getName);
         movie.sort(cmp);
-//        movie.sort(cmp1);
         for (Movie m : movie) {
             System.out.println(m);
         }
-        movieList.containsKey("Sherlock");
+        System.out.println("-------------------------------------------YearReversed------------------------");
     }
+
     public static void runReverse(){
         Map<String, List<Movie>> movieList = MovieService.readFile();
         List<Movie> movie = movieList.get("movies");
         Comparator<Movie> cmp = Comparator.comparingInt(Movie::getYear);
+        movie.sort(cmp.reversed());
+        for (Movie m : movie) {
+            System.out.println(m);
+        }
+        System.out.println("-------------------------------------------Name Of Movie------------------------");
+    }
+
+    public static void runName(){
+        Map<String, List<Movie>> movieList = MovieService.readFile();
+        List<Movie> movie = movieList.get("movies");
+        Comparator<Movie> cmp = Comparator.comparing(Movie::getName);
         movie.sort(cmp);
         for (Movie m : movie) {
             System.out.println(m);
         }
+        System.out.println("-------------------------------------------NameReversed------------------------");
     }
+
+    public static void runReversedName(){
+        Map<String, List<Movie>> movieList = MovieService.readFile();
+        List<Movie> movie = movieList.get("movies");
+        Comparator<Movie> cmp = Comparator.comparing(Movie::getName);
+        movie.sort(cmp.reversed());
+        for (Movie m : movie) {
+            System.out.println(m);
+        }
+        System.out.println("-------------------------------------------Director Name------------------------");
+    }
+
+    public static void runDirectorName(){
+        Map<String, List<Movie>> movieList = MovieService.readFile();
+        List<Movie> movie = movieList.get("movies");
+//        Comparator<Movie> cmp = Comparator.comparing(Movie::getDirector);
+//        movie.sort(cmp.reversed());
+        for (Movie m : movie) {
+            System.out.println(m);
+        }
+        System.out.println("-------------------------------------------NameReversed------------------------");
+    }
+
 }
